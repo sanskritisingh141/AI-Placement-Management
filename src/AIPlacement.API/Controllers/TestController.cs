154ef04fcb3f7ab65626802878337a5027a6eead
Sanskriti Singh <sanskritisingh0141@gmail.com>
2026-08-19 00:24:46 +0530

    Add initial Web API test endpoint

---
+++ src/AIPlacement.API/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AIPlacement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "AI Placement Management API is working"
+            });
+        }
+    }
+}
